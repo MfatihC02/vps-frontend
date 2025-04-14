a3f4fbe7ea221ad2a7d37fa4d87d5154fba65eaf
MfatihC02 <139883287+MfatihC02@users.noreply.github.com>
2025-04-14 15:31:39 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,30 @@
 <template>
-  <footer class="bg-gray-900 text-gray-300 h-[600px] md:h-[500px] w-full flex flex-col">
+  <footer
+    class="bg-gray-900 text-gray-300 h-[600px] md:h-[500px] w-full flex flex-col"
+  >
     <!-- Üst Footer -->
     <div class="container mx-auto px-4 py-8 flex-1">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 flex-grow">
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 flex-grow"
+      >
         <!-- Kurumsal -->
         <div class="space-y-4">
-          <button 
-            @click="toggleSection('kurumsal')" 
+          <button
+            @click="toggleSection('kurumsal')"
             class="w-full flex items-center justify-between text-lg font-semibold text-white mb-4 sm:cursor-default"
           >
             <span>Kurumsal</span>
             <iconify-icon
               icon="mdi:chevron-down"
-              :class="{'rotate-180': sections.kurumsal}"
+              :class="{ 'rotate-180': sections.kurumsal }"
               class="sm:hidden transition-transform duration-300"
               width="24"
             ></iconify-icon>
           </button>
-          <ul :class="{'hidden sm:block': !sections.kurumsal}" class="space-y-2">
+          <ul
+            :class="{ 'hidden sm:block': !sections.kurumsal }"
+            class="space-y-2"
+          >
             <li>
               <router-link
                 to="/hakkimizda"
@@ -75,19 +82,22 @@
 
         <!-- Müşteri Hizmetleri -->
         <div class="space-y-4">
-          <button 
-            @click="toggleSection('musteri')" 
+          <button
+            @click="toggleSection('musteri')"
             class="w-full flex items-center justify-between text-lg font-semibold text-white mb-4 sm:cursor-default"
           >
             <span>Müşteri Hizmetleri</span>
             <iconify-icon
               icon="mdi:chevron-down"
-              :class="{'rotate-180': sections.musteri}"
+              :class="{ 'rotate-180': sections.musteri }"
               class="sm:hidden transition-transform duration-300"
               width="24"
             ></iconify-icon>
           </button>
-          <ul :class="{'hidden sm:block': !sections.musteri}" class="space-y-2">
+          <ul
+            :class="{ 'hidden sm:block': !sections.musteri }"
+            class="space-y-2"
+          >
             <li>
               <router-link
                 to="/faq"
@@ -132,30 +142,36 @@
 
         <!-- İletişim Bilgileri -->
         <div class="space-y-4">
-          <button 
-            @click="toggleSection('iletisim')" 
+          <button
+            @click="toggleSection('iletisim')"
             class="w-full flex items-center justify-between text-lg font-semibold text-white mb-4 sm:cursor-default"
           >
             <span>İletişim Bilgileri</span>
             <iconify-icon
               icon="mdi:chevron-down"
-              :class="{'rotate-180': sections.iletisim}"
+              :class="{ 'rotate-180': sections.iletisim }"
               class="sm:hidden transition-transform duration-300"
               width="24"
             ></iconify-icon>
           </button>
-          <ul :class="{'hidden sm:block': !sections.iletisim}" class="space-y-3">
+          <ul
+            :class="{ 'hidden sm:block': !sections.iletisim }"
+            class="space-y-3"
+          >
             <li class="flex items-start space-x-3">
               <iconify-icon
                 icon="mdi:map-marker"
                 class="text-green-500"
                 width="20"
               ></iconify-icon>
-              <span>Şükran Mah. Sahibi Ata Cad. No:38 Meram, Konya</span>
+              <span>
+                MUSALLA BAĞLARI MAH, Yeni İstanbul Cd. NO:8 Selçuklu,
+                Konya</span
+              >
             </li>
             <li>
-              <a 
-                href="tel:03323513128"
+              <a
+                href="tel:0332 235 11 28"
                 class="flex items-center space-x-3 hover:text-green-500 transition-colors duration-200"
               >
                 <iconify-icon
@@ -167,7 +183,7 @@
               </a>
             </li>
             <li>
-              <a 
+              <a
                 href="mailto:iletisim@tarimsepetim.com.tr"
                 class="flex items-center space-x-3 hover:text-green-500 transition-colors duration-200"
               >
@@ -198,7 +214,7 @@
           <h3 class="text-lg font-semibold text-white mb-6">Sosyal Medya</h3>
           <!-- Sosyal Medya İkonları -->
           <div class="flex space-x-4 mb-6">
-            <a 
+            <a
               href="https://www.instagram.com/tarimsepetim"
               class="transform hover:scale-110 transition-transform duration-200"
             >
@@ -218,7 +234,7 @@
                 width="28"
               ></iconify-icon>
             </a>
-            <a 
+            <a
               href="#"
               class="transform hover:scale-110 transition-transform duration-200"
             >
@@ -230,22 +246,26 @@
             </a>
           </div>
           <!-- WhatsApp Button -->
-          <a 
+          <a
             href="https://wa.me/905516419012"
             target="_blank"
             rel="noopener noreferrer"
             class="whatsapp-button flex items-center gap-3 bg-[#25D366] hover:bg-[#20BD5A] text-white p-3 rounded-lg w-full justify-center sm:justify-start"
           >
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              width="28" 
-              height="28" 
-              viewBox="0 0 24 24" 
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
               class="fill-current"
             >
-              <path d="M12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01A9.816 9.816 0 0 0 12.04 2m.01 1.67c2.2 0 4.26.86 5.82 2.42a8.225 8.225 0 0 1 2.41 5.83c0 4.54-3.7 8.23-8.24 8.23c-1.48 0-2.93-.39-4.19-1.15l-.3-.17l-3.12.82l.83-3.04l-.2-.32a8.188 8.188 0 0 1-1.26-4.38c.01-4.54 3.7-8.24 8.25-8.24M8.53 7.33c-.16 0-.43.06-.66.31c-.22.25-.87.86-.87 2.07c0 1.22.89 2.39 1 2.56c.14.17 1.76 2.67 4.25 3.73c.59.27 1.05.42 1.41.53c.59.19 1.13.16 1.56.1c.48-.07 1.46-.6 1.67-1.18c.21-.58.21-1.07.15-1.18c-.07-.1-.23-.16-.48-.27c-.25-.14-1.47-.74-1.69-.82c-.23-.08-.37-.12-.56.12c-.16.25-.64.81-.78.97c-.15.17-.29.19-.53.07c-.26-.13-1.06-.39-2-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.12-.24-.01-.39.11-.5c.11-.11.27-.29.37-.44c.13-.14.17-.25.25-.41c.08-.17.04-.31-.02-.43c-.06-.11-.56-1.35-.77-1.84c-.2-.48-.4-.42-.56-.43c-.14 0-.3-.01-.47-.01z"/>
+              <path
+                d="M12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01A9.816 9.816 0 0 0 12.04 2m.01 1.67c2.2 0 4.26.86 5.82 2.42a8.225 8.225 0 0 1 2.41 5.83c0 4.54-3.7 8.23-8.24 8.23c-1.48 0-2.93-.39-4.19-1.15l-.3-.17l-3.12.82l.83-3.04l-.2-.32a8.188 8.188 0 0 1-1.26-4.38c.01-4.54 3.7-8.24 8.25-8.24M8.53 7.33c-.16 0-.43.06-.66.31c-.22.25-.87.86-.87 2.07c0 1.22.89 2.39 1 2.56c.14.17 1.76 2.67 4.25 3.73c.59.27 1.05.42 1.41.53c.59.19 1.13.16 1.56.1c.48-.07 1.46-.6 1.67-1.18c.21-.58.21-1.07.15-1.18c-.07-.1-.23-.16-.48-.27c-.25-.14-1.47-.74-1.69-.82c-.23-.08-.37-.12-.56.12c-.16.25-.64.81-.78.97c-.15.17-.29.19-.53.07c-.26-.13-1.06-.39-2-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.12-.24-.01-.39.11-.5c.11-.11.27-.29.37-.44c.13-.14.17-.25.25-.41c.08-.17.04-.31-.02-.43c-.06-.11-.56-1.35-.77-1.84c-.2-.48-.4-.42-.56-.43c-.14 0-.3-.01-.47-.01z"
+              />
             </svg>
-            <span class="font-semibold text-base" style="contain: content;">WhatsApp'tan Sipariş Ver</span>
+            <span class="font-semibold text-base" style="contain: content"
+              >WhatsApp'tan Sipariş Ver</span
+            >
           </a>
         </div>
       </div>
@@ -273,16 +293,17 @@
 
         <!-- Copyright -->
         <div class="border-t border-gray-700 pt-4 mt-2">
-          <div class="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+          <div
+            class="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0"
+          >
             <div class="text-sm text-gray-400">
-              &copy; {{ new Date().getFullYear() }} TARİMSEPETİM -
-              Mühendisler Ticaret Kollektif Şirketi'nin tescilli markasıdır.
-              Tüm hakları saklıdır.
+              &copy; {{ new Date().getFullYear() }} TARİMSEPETİM - Mühendisler
+              Ticaret Kollektif Şirketi'nin tescilli markasıdır. Tüm hakları
+              saklıdır.
             </div>
             <div class="flex items-center space-x-2 text-sm text-gray-400">
-             
               <span class="mr-2">Tasarım & Geliştirme:</span>
-              <a 
+              <a
                 href="https://www.linkedin.com/in/fatih-%C3%A7a%C4%9F%C4%B1rgan-151b892a5/"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -320,12 +341,13 @@ const showScrollTop = ref(false);
 const sections = ref({
   kurumsal: false,
   musteri: false,
-  iletisim: false
+  iletisim: false,
 });
 
 // Toggle section visibility
 const toggleSection = (section) => {
-  if (window.innerWidth < 640) { // Sadece mobil görünümde çalışsın
+  if (window.innerWidth < 640) {
+    // Sadece mobil görünümde çalışsın
     sections.value[section] = !sections.value[section];
   }
 };
